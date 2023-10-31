61c9943735dcf9bec13ec288c0dd492f1f9fb8be
RiivoMeremaa <riivo.meremaa@voco.ee>
2023-10-31 17:11:09 +0200

    Väljastuse näited

---
+++ print/examples/ex1.py
@@ -0,0 +1,29 @@
+print("Hello world!")
+
+print("Welcome")
+print("to school!")
+
+print("""Welcome
+to school""")
+
+print("Hello, I am your program!")
+name = input("What is your name: ")
+print(f"Hello {name}!")
+
+print("Hi", end=" ")
+print("Riivo!")
+
+print("first", "second")
+print(1, 2)
+print("My age is", 18)
+print("My age is " + str(18))
+
+name = "Ago"
+print(f"Hi {name}!")
+price_without_tax = 20
+print(f"Price with tax is {price_without_tax * 1.2}")
+print("Sum: {1 + 2 + 3}")
+print(f"Sum: {1 + 2 + 3}")
+
+name = input("Enter your name:")
+print(f"Hi {name}!")
